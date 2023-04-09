865b69abb8e5833cf313ac51dd99030ca4e38dba
Kevin <kevinfiorentino.1294@gmail.com>
2023-04-09 17:27:34 -0300

    Wallet connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: []
 })
 export class AppComponent {
-  title = 'angular-phantom';
+
+  public address: string = '';
+  public phantom?: PhantomWalletAdapter;
+
+  async connectWallet() {
+    this.phantom = new PhantomWalletAdapter();
+    await this.phantom.connect();
+
+    if (this.phantom && this.phantom.publicKey) {
+      this.address = this.phantom.publicKey.toString();
+      this.listeningChangeWallet();
+    }
+  }
+
+  async disconnectWallet() {
+    await this.phantom?.disconnect();
+    this.address = '';
+  }
+
+  listeningChangeWallet() {
+    this.phantom?.on('connect', (publicKey) => {
+      if (publicKey) {
+        this.address = publicKey.toString();
+        console.log('New Address', this.address)
+      }
+    });
+  }
+
 }
